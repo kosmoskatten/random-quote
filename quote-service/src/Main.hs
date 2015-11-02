b260e752e94c2ca1598dae81acf6da4fb400d029
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-11-02 23:08:07 +0100

    Added very simple service.

---
+++ quote-service/src/Main.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Main
+    ( main
+    ) where
+
+import Data.Aeson
+import Data.Text (Text)
+import GHC.Generics
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
+import qualified Data.ByteString.Char8 as BS
+
+data Quote =
+    Quote { author :: !Text
+          , imgUrl :: !Text
+          , quote  :: !Text
+          }
+    deriving (Generic, Show)
+
+instance ToJSON Quote
+
+main :: IO ()
+main = run 8080 router
+
+router :: Application
+router request responseReceived = do
+    response <-
+        case rawPathInfo request of
+            "/api/random-quote" -> do
+                quote' <- getRandomQuote
+                return $ responseLBS status200 [ mediaJson ] (encode quote')
+            _                 -> serveDirectory "static" request
+
+    responseReceived response
+
+getRandomQuote :: IO Quote
+getRandomQuote = return $ Quote "Linus Torvalds"
+                                "Linus-Torvalds.jpg"
+                                "Talk is cheap. Show me the code."
+
+serveDirectory :: FilePath -> Request -> IO Response
+serveDirectory directory request = do
+    let reqFile = BS.unpack (rawPathInfo request)
+        path    = directory `mappend` "/" `mappend` reqFile
+    return $ responseFile status200 [] path Nothing
+
+mediaJson :: Header
+mediaJson = ("Content-Type", "application/json")
